002a8cd1eaf2258ce5ff0b5343bc369407ff7698
Henry Hsu <hhsu@zendesk.com>
2014-03-14 18:50:03 -0700

    Get travis build to work.

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = ENV['SECRET_TOKEN']
+Dummy::Application.config.secret_token = '717b5a4e715127fc72bb840a70685d6e'
