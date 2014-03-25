82dac05c3a2929b04b669416bde1e834c164762d
Marten Lienen <marten.lienen@gmail.com>
2014-03-25 22:34:02 +0100

    Extract methods

--- lib/browserify-rails/directive_processor.rb
+++ lib/browserify-rails/directive_processor.rb
@@ -1,11 +1,11 @@
-require 'open3'
-require 'json'
+require "open3"
+require "json"
 
 module BrowserifyRails
-  class DirectiveProcessor < Sprockets::DirectiveProcessor
-    BROWSERIFY_CMD  = './node_modules/.bin/browserify'.freeze
-    MODULE_DEPS_CMD = './node_modules/.bin/module-deps'.freeze
-    COFFEEIFY_PATH  = './node_modules/coffeeify'.freeze
+  class DirectiveProcessor < Tilt::Template
+    BROWSERIFY_CMD  = "./node_modules/.bin/browserify".freeze
+    MODULE_DEPS_CMD = "./node_modules/.bin/module-deps".freeze
+    COFFEEIFY_PATH  = "./node_modules/coffeeify".freeze
 
     class BrowserifyError < RuntimeError
     end
@@ -13,41 +13,65 @@ module BrowserifyRails
     class ModuleDepsError < RuntimeError
     end
 
+    def prepare
+    end
+
     def evaluate(context, locals, &block)
-      super
-
-      if commonjs_module?(data)
-        browserify_cmd = File.join(context.environment.root, BROWSERIFY_CMD)
-        module_deps_cmd = File.join(context.environment.root, MODULE_DEPS_CMD)
-
-        raise ArgumentError, "#{browserify_cmd} could not be found. Please run npm install." unless File.exist?(browserify_cmd)
-        raise ArgumentError, "#{module_deps_cmd} could not be found. Please run npm install." unless File.exist?(module_deps_cmd)
-
-        deps = JSON.parse(run_command("#{module_deps_cmd} #{pathname}"))
-        deps.each do |dep|
-          path = File.basename(dep['id'], context.environment.root)
-          next if path == File.basename(pathname)
+      if commonjs_module?
+        dependencies.each do |dep|
+          path = File.basename(dep["id"], context.environment.root)
+          next if path == File.basename(file)
 
           if path =~ /<([^>]+)>/
             path = $1
           else
-            path = "./#{path}" unless relative?(path)
+            path = "./#{path}" unless path.start_with?(".")
           end
 
           context.depend_on_asset(path)
         end
 
-        params = "-d"
-        params += " -t coffeeify --extension='.coffee'" if File.directory?(COFFEEIFY_PATH)
-
-        run_command("#{browserify_cmd} #{params} #{pathname}")
+        browserify
       else
         data
       end
     end
 
-    def commonjs_module?(data)
-      data.to_s.include?('module.exports') || data.to_s.include?('require')
+    private
+
+    def commonjs_module?
+      data.to_s.include?("module.exports") || data.to_s.include?("require")
+    end
+
+    def dependencies
+      JSON.parse(run_command("#{module_deps_cmd} #{file}"))
+    end
+
+    def browserify
+      params = "-d"
+      params += " -t coffeeify --extension='.coffee'" if File.directory?(COFFEEIFY_PATH)
+
+      run_command("#{browserify_cmd} #{params} #{file}")
+    end
+
+    def browserify_cmd
+      cmd = File.join(Rails.root, BROWSERIFY_CMD)
+
+      if !File.exist?(cmd)
+        raise ArgumentError, "#{cmd} could not be found. Please run npm install."
+      end
+
+      cmd
+    end
+
+    def module_deps_cmd
+      cmd = File.join(Rails.root, MODULE_DEPS_CMD)
+
+      if !File.exist?(cmd)
+        raise ArgumentError, "#{cmd} could not be found. Please run npm install."
+      end
+
+      cmd
     end
 
     def run_command(command)
