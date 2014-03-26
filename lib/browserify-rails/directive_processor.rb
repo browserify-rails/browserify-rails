83bf61d23d678ed32cea26746acc5b5cd6a41d29
Marten Lienen <marten.lienen@gmail.com>
2014-03-26 03:26:44 +0100

    Refactor

--- lib/browserify-rails/directive_processor.rb
+++ lib/browserify-rails/directive_processor.rb
@@ -41,17 +41,17 @@ module BrowserifyRails
 
     # @return [<String>] Paths of files, that this file depends on
     def dependencies
-      run_with_data("#{browserify_cmd} --list").lines.map(&:strip).select do |path|
+      run_browserify("--list").lines.map(&:strip).select do |path|
         # Filter the temp file, where browserify caches the input stream
         File.exists?(path)
       end
     end
 
     def browserify
-      params = "-d"
-      params += " -t coffeeify --extension='.coffee'" if File.directory?(COFFEEIFY_PATH)
+      options = "-d"
+      options += " -t coffeeify --extension='.coffee'" if File.directory?(COFFEEIFY_PATH)
 
-      run_with_data("#{browserify_cmd} #{params}")
+      run_browserify(options)
     end
 
     def browserify_cmd
@@ -64,7 +64,7 @@ module BrowserifyRails
       cmd
     end
 
-    # Run `command` with `data` on standard input.
+    # Run browserify with `data` on standard input.
     #
     # We are passing the data via stdin, so that earlier preprocessing steps are
     # respected. If you had, say, an "application.js.coffee.erb", passing the
@@ -73,11 +73,13 @@ module BrowserifyRails
     # behavior of success, because everything has been compiled to plain
     # javascript at the time this processor is called.
     #
-    # @raise [BrowserifyError] if `command` does not succeed
-    # @param command [String]
+    # @raise [BrowserifyError] if browserify does not succeed
+    # @param options [String] Options for browserify
     # @return [String] Output on standard out
-    def run_with_data(command)
-      stdout, stderr, status = Open3.capture3(command, stdin_data: data, chdir: File.dirname(file))
+    def run_browserify(options)
+      command = "#{browserify_cmd} #{options}"
+      directory = File.dirname(file)
+      stdout, stderr, status = Open3.capture3(command, stdin_data: data, chdir: directory)
 
       if !status.success?
         raise BrowserifyError.new("Error while running `#{command}`:\n\n#{stderr}")
