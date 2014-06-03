06c6263c9c424cf1f5e7cf895f1c5fadd586ad4e
Marten Lienen <marten.lienen@gmail.com>
2014-06-03 11:02:00 +0200

    Test browserification of coffee files

--- browserify-rails.gemspec
+++ browserify-rails.gemspec
@@ -23,5 +23,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rails", "~> 3.2"
+  spec.add_development_dependency "coffee-rails"
   spec.add_development_dependency "mocha"
+  spec.add_development_dependency "pry"
 end
--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -1,13 +1,13 @@
-require 'test_helper'
+require "test_helper"
 
 class BrowserifyTest < ActionController::IntegrationTest
-
   setup do
     Rails.application.assets.cache = nil
 
-    FileUtils.cp(File.join(Rails.root, 'app/assets/javascripts/application.js.example'), File.join(Rails.root, 'app/assets/javascripts/application.js'))
-    FileUtils.cp(File.join(Rails.root, 'app/assets/javascripts/foo.js.example'), File.join(Rails.root, 'app/assets/javascripts/foo.js'))
-    FileUtils.cp(File.join(Rails.root, 'app/assets/javascripts/nested/index.js.example'), File.join(Rails.root, 'app/assets/javascripts/nested/index.js'))
+    FileUtils.cp(File.join(Rails.root, "app/assets/javascripts/application.js.example"), File.join(Rails.root, "app/assets/javascripts/application.js"))
+    FileUtils.cp(File.join(Rails.root, "app/assets/javascripts/foo.js.example"), File.join(Rails.root, "app/assets/javascripts/foo.js"))
+    FileUtils.cp(File.join(Rails.root, "app/assets/javascripts/nested/index.js.example"), File.join(Rails.root, "app/assets/javascripts/nested/index.js"))
+    FileUtils.cp(File.join(Rails.root, "app/assets/javascripts/mocha.js.coffee.example"), File.join(Rails.root, "app/assets/javascripts/mocha.js.coffee"))
   end
 
   test "asset pipeline should serve application.js" do
@@ -36,7 +36,7 @@ class BrowserifyTest < ActionController::IntegrationTest
     # Ensure that Sprockets can detect the change to the file modification time
     sleep 1
 
-    File.open(File.join(Rails.root, 'app/assets/javascripts/foo.js'), 'w+') do |f|
+    File.open(File.join(Rails.root, "app/assets/javascripts/foo.js"), "w+") do |f|
       f.puts "require('./nested');"
       f.puts "module.exports = function (n) { return n * 12 }"
     end
@@ -58,7 +58,7 @@ class BrowserifyTest < ActionController::IntegrationTest
     # Ensure that Sprockets can detect the change to the file modification time
     sleep 1
 
-    File.open(File.join(Rails.root, 'app/assets/javascripts/application.js'), 'w+') do |f|
+    File.open(File.join(Rails.root, "app/assets/javascripts/application.js"), "w+") do |f|
       f.puts "var foo = require('./foo');"
       f.puts "console.log(foo(11));"
     end
@@ -70,8 +70,17 @@ class BrowserifyTest < ActionController::IntegrationTest
     assert_equal expected_output, @response.body.strip
   end
 
+  test "browserifies coffee files after they have been compiled to JS" do
+    expected_output = fixture("mocha.js")
+
+    get "/assets/mocha.js"
+
+    assert_response :success
+    assert_equal expected_output, @response.body.strip
+  end
+
   test "throws BrowserifyError if something went wrong while executing browserify" do
-    File.open(File.join(Rails.root, 'app/assets/javascripts/application.js'), 'w+') do |f|
+    File.open(File.join(Rails.root, "app/assets/javascripts/application.js"), "w+") do |f|
       f.puts "var foo = require('./foo');"
       f.puts "var bar = require('./bar');"
     end
